ec20bc4be8e4fae8edb09e8fd013c78f81405103
gignsky <gig@gignsky.com>
2025-05-26 16:40:48 -0400

    added config to make only if enabled

--- nix/modules/nixos-module.nix
+++ nix/modules/nixos-module.nix
@@ -1,1 +1,14 @@
-
+{ config, pkgs, lib, ... }:
+let
+  username = config.username;
+  group = config.users.users.${username}.group or "users";
+  home = config.users.users.${username}.home;
+in
+{
+  options = import ../../config-options.nix { inherit lib; };
+  config = lib.mkIf config.programs.neofetch.enable {
+    environment.systemPackages = with pkgs; [
+      neofetch
+    ];
+  };
+}
