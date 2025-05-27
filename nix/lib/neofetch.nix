17dcc7cf15e0656916b00a99e39e98a3fb679502
gignsky <gig@gignsky.com>
2025-05-26 20:25:43 -0400

    moved some things around

--- nix/lib/neofetch.nix
+++ nix/lib/neofetch.nix
@@ -1,13 +1,8 @@
 { config, lib, pkgs, ... }:
-let
-  configOptions = import ./config-options.nix { inherit lib; };
-  cfg = config.programs.nufetch;
-  configFile = pkgs.writeText "nufetch.conf" (import ./config-maker.nix { inherit cfg lib; });
-in
 {
   options = import ./config-options.nix { inherit lib; };
   config = lib.mkIf config.programs.nufetch.enable {
     environment.systemPackages = [ pkgs.neofetch ];
-    environment.etc."nufetch.conf".source = configFile;
+    # environment.etc."nufetch.conf".source = configFile;
   };
 }
--- nix/modules/nixos-module.nix
+++ nix/modules/nixos-module.nix
@@ -1,22 +1,19 @@
 { config, pkgs, lib, ... }:
 let
-  nufetchCfg = config.programs.nufetch;
-  enabledFields = lib.filterAttrs (_: v: v == true) (lib.removeAttrs nufetchCfg [ "enable" "extraFields" ]);
-  extraFields = nufetchCfg.extraFields;
-  configFile = pkgs.writeText "nufetch.conf"
-    (import ../lib/config-maker.nix { cfg = nufetchCfg; lib = lib; });
+  cfg = config.programs.nufetch;
+  configFile = pkgs.writeText "nufetch.conf" (import ../lib/config-maker.nix { inherit cfg lib; });
 in
 {
   imports = [ ../lib/neofetch.nix ];
   config = lib.mkIf config.programs.nufetch.enable {
-    environment.systemPackages = [
-      (pkgs.writeShellScriptBin "neofetch" ''
-        exec ${pkgs.neofetch}/bin/neofetch --config /etc/nufetch.conf "$@"
-      '')
-      (pkgs.writeShellScriptBin "nufetch" ''
-        exec ${pkgs.neofetch}/bin/neofetch --config /etc/nufetch.conf "$@"
-      '')
-    ];
+    # environment.systemPackages = [
+    #   (pkgs.writeShellScriptBin "neofetch" ''
+    #     exec ${pkgs.neofetch}/bin/neofetch --config /etc/nufetch.conf "$@"
+    #   '')
+    #   (pkgs.writeShellScriptBin "nufetch" ''
+    #     exec ${pkgs.neofetch}/bin/neofetch --config /etc/nufetch.conf "$@"
+    #   '')
+    # ];
     environment.etc."nufetch.conf".source = configFile;
   };
 }
