01b3601bcfe6012395bb99b24f62d399ac135004
gignsky <gig@gignsky.com>
2025-05-26 21:58:50 -0400

    got patches semi-working

--- nix/flake/modules/packages.nix
+++ nix/flake/modules/packages.nix
@@ -1,6 +1,15 @@
 { inputs, ... }:
 {
-  perSystem = { pkgs, ... }: {
-    packages.neofetch-nixos-module = pkgs.neofetch;
-  };
+  perSystem = { pkgs, system, ... }:
+    let
+      patchedPkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          (import ../../overlays/neofetch-patch-nixos-module.nix)
+        ];
+      };
+    in
+    {
+      packages.neofetch-nixos-module = patchedPkgs.neofetch;
+    };
 }
