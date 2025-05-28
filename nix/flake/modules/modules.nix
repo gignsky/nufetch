95e1a5f17af7b050ce151a92427c0d9352b6b506
gignsky <gig@gignsky.com>
2025-05-28 04:54:06 -0400

    added basic home-manager module

--- nix/flake/modules/modules.nix
+++ nix/flake/modules/modules.nix
@@ -10,4 +10,15 @@
     };
     default = self: self.nufetch;
   };
+
+  flake.homeManagerModules = {
+    nufetch = {
+      imports = [
+        ../../modules/home-manager-module.nix
+        ../../lib/config-options.nix
+        # ../../lib/config-maker.nix
+      ];
+    };
+    default = self: self.nufetch;
+  };
 }
---
+++ nix/modules/home-manager-module.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.programs.nufetch;
+  configFile = pkgs.writeText "nufetch.conf" (import ../lib/config-maker.nix { inherit cfg lib; });
+in
+{
+  imports = [ ../lib/neofetch.nix ];
+  config = lib.mkIf config.programs.nufetch.enable {
+    environment.systemPackages = [
+      #   (pkgs.writeShellScriptBin "neofetch" ''
+      #     exec ${pkgs.neofetch}/bin/neofetch --config /etc/nufetch.conf "$@"
+      #   '')
+      (pkgs.writeShellScriptBin "nufetch" ''
+        exec ${pkgs.neofetch}/bin/neofetch "$@"
+      '')
+      pkgs.neofetch
+    ];
+    home.".config/neofetch/neofetch.conf".source = configFile;
+  };
+}
