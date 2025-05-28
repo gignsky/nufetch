c84e918fd6b32962bf200b2612c2291e27a96497
gignsky <gig@gignsky.com>
2025-05-28 04:27:18 -0400

    updated system call

--- nix/modules/nixos-module.nix
+++ nix/modules/nixos-module.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, outputs, lib, ... }:
+{ config, pkgs, outputs, system, lib, ... }:
 let
   cfg = config.programs.nufetch;
   configFile = pkgs.writeText "nufetch.conf" (import ../lib/config-maker.nix { inherit cfg lib; });
@@ -11,9 +11,9 @@ in
       #     exec ${pkgs.neofetch}/bin/neofetch --config /etc/nufetch.conf "$@"
       #   '')
       (pkgs.writeShellScriptBin "nufetch" ''
-        exec ${outputs.packages.${pkgs.system}.neofetch-nixos-module}/bin/neofetch --config /etc/nufetch.conf "$@"
+        exec ${outputs.packages.${system}.neofetch-nixos-module}/bin/neofetch --config /etc/nufetch.conf "$@"
       '')
-      outputs.packages.${pkgs.system}.neofetch-nixos-module
+      outputs.packages.${system}.neofetch-nixos-module
     ];
     environment.etc."nufetch.conf".source = configFile;
   };
